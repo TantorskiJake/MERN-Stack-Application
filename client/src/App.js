3822be5cff06cbe37316395ef137e7a55e9ef737
Jake Tantorski <jake.tantorski1@marist.edu>
2024-03-07 16:47:32 -0500

    created pages folder with page files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
-  return  <div className="App"></div>;
+  return ( 
+  <div className="App"> 
+    <Router>
+      <Routes>
+        <Route path="/" element = {} />
+      </Routes> 
+    </Router>
+  </div>
+  );
 }
-
+ 
 export default App;
