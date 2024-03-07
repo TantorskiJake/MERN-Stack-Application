a557b01a6b11b28d16b121e9657338b1bc283aa2
Jake Tantorski <jake.tantorski1@marist.edu>
2024-03-07 15:17:34 -0500

    created user route

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors"
+import mongoose from "mongoose"
+
+import {userRouter} from './routes/users.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter); 
+
+mongoose.connect(
+    "mongodb+srv://jaketech:MERNpassword123@recipes.pfdwpew.mongodb.net/recipes?retryWrites=true&w=majority&appName=recipes"
+);
+
+
+app.listen(3001, () => console.log("Server Started"));
