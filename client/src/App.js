fb2c634e56071c7cd1383dd3990e3a978c86cf4a
Jake Tantorski <jake.tantorski1@marist.edu>
2024-03-07 16:52:56 -0500

    created routes on the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Auth } from "./pages/auth";
+import { CreateRecipe } from "./pages/create-recipe";
+import { SavedRecipe } from "./pages/saved-recipes";
 
 function App() {
-  return ( 
-  <div className="App"> 
-    <Router>
-      <Routes>
-        <Route path="/" element = {} />
-      </Routes> 
-    </Router>
-  </div>
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipe" element={<SavedRecipe />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
- 
+
 export default App;
