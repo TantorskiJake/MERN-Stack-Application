475468f2121e24e53f6781b220fcd1b954ae84b7
Jake Tantorski <jake.tantorski1@marist.edu>
2024-03-07 21:42:16 -0500

    login, logout and register all working!

--- client/src/components/navabr.js
+++ client/src/components/navabr.js
@@ -1,8 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
 export const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigate("/auth");
+  };
   return (
     <div className="navbar">
       <Link to="/"> Home</Link>
@@ -11,7 +18,7 @@ export const Navbar = () => {
       {!cookies.access_token ? (
         <Link to="/auth"> Login/Register</Link>
       ) : (
-        <button> Logout </button>
+        <button onClick={logout}> Logout </button>
       )}
     </div>
   );
