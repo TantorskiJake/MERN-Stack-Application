f17476573863ecf1a9b15cc752328499fbc5135d
Jake Tantorski <jake.tantorski1@marist.edu>
2024-03-07 21:35:45 -0500

    added logout button

--- client/src/components/navabr.js
+++ client/src/components/navabr.js
@@ -1,12 +1,18 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 export const Navbar = () => {
-    return (
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  return (
     <div className="navbar">
-        <Link to="/"> Home</Link>
-        <Link to="/create-recipe"> Create Recipe</Link>
-        <Link to="/saved-recipes"> Saved Recipes</Link>
+      <Link to="/"> Home</Link>
+      <Link to="/create-recipe"> Create Recipe</Link>
+      <Link to="/saved-recipes"> Saved Recipes</Link>
+      {!cookies.access_token ? (
         <Link to="/auth"> Login/Register</Link>
+      ) : (
+        <button> Logout </button>
+      )}
     </div>
   );
 };
